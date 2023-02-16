208d72c231784d68ec7ec46ff2dd54b5a92dee85
B3njam1n3012 <tankerkzheng1@gmail.com>
2023-02-17 01:11:30 +0800

    Update User.java

--- src/main/java/Data/User.java
+++ src/main/java/Data/User.java
@@ -1,59 +1,42 @@
 package Data;
 
 public class User {
-	
 	private String userId, name,  email, password, age, role;
-	
-	
 
 	public String getUserId() {
 		return userId;
 	}
-
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getAge() {
 		return age;
 	}
-
 	public void setAge(String age) {
 		this.age = age;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getRole() {
 		return role;
 	}
-
 	public void setRole(String role) {
 		this.role = role;
 	}
-	
-	
-
 }
