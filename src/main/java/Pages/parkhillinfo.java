ef4ebe97cf4335b8e5e01e7d3dca609b79ce2fc1
B3njam1n3012 <tankerkzheng1@gmail.com>
2023-02-16 23:13:54 +0800

    Update parkhillinfo.java

--- src/main/java/Pages/parkhillinfo.java
+++ src/main/java/Pages/parkhillinfo.java
@@ -70,12 +70,12 @@ public class parkhillinfo {
 		lblTitle.setBounds(900, 80, 400, 42);
 		info.getContentPane().add(lblTitle);
                 
-                JLabel lblEmail = new JLabel("Number of Floors: ");
+                JLabel lblEmail = new JLabel("Number of Floors: 68");
 		lblEmail.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 17));
 		lblEmail.setBounds(900, 130, 315, 42);
 		info.getContentPane().add(lblEmail);
                 
-                JLabel lblUnits = new JLabel("Number of Units: ");
+                JLabel lblUnits = new JLabel("Number of Units: 1668");
 		lblUnits.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 17));
 		lblUnits.setBounds(900, 180, 315, 42);
 		info.getContentPane().add(lblUnits);
@@ -85,13 +85,26 @@ public class parkhillinfo {
 		lblFac.setBounds(900, 230, 315, 42);
 		info.getContentPane().add(lblFac);
                 
-           
-                JLabel lblLocation = new JLabel("""
-                                                Location: Jln Jalil Perkasa 19,
-                                                Bukit Jalil, 57000 Kuala Lumpur""");
+                JTextArea lblFacc = new JTextArea("Swimming Pool, Gym, Multipurpose Hall, Covered Parking, Club House, Playground, 24-hour Security");
+		lblFacc.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 12));
+		lblFacc.setBounds(900, 280, 315, 42);
+                lblFacc.setLineWrap(true);
+                lblFacc.setWrapStyleWord(true);
+                lblFacc.setEditable(false);
+		info.getContentPane().add(lblFacc);
+                
+                JLabel lblLocation = new JLabel("Location:");
 		lblLocation.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 17));
-		lblLocation.setBounds(900, 280, 500, 100);
+		lblLocation.setBounds(900, 330, 315, 42);
 		info.getContentPane().add(lblLocation);
+                
+                JTextArea lblAdd = new JTextArea("Jln Jalil Perkasa 19, Bukit Jalil, 57000 Kuala Lumpur");
+		lblAdd.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 12));
+		lblAdd.setBounds(900, 380, 315, 42);
+                lblAdd.setLineWrap(true);
+                lblAdd.setWrapStyleWord(true);
+                lblAdd.setEditable(false);
+		info.getContentPane().add(lblAdd);
                 
         }
 }
