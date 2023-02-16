719f3dcf0242b79f575c819a05803d8593c64f89
B3njam1n3012 <tankerkzheng1@gmail.com>
2023-02-16 22:19:31 +0800

    test

---
+++ src/main/java/Pages/parkhillinfo.java
@@ -0,0 +1,77 @@
+package Pages;
+
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import Handlers.CRUD;
+import java.awt.BorderLayout;
+
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.swing.ImageIcon;
+
+public class parkhillinfo {
+
+	public JFrame info;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					parkhillinfo window = new parkhillinfo();
+					window.info.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public parkhillinfo() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		info = new JFrame();
+		info.setTitle("Parkhill Residences Info");
+		info.setBounds(100, 100, 871, 700);
+		info.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		info.getContentPane().setLayout(null);
+                
+                ImageIcon imageIcon = new ImageIcon("parkhill.jpg");
+                JLabel parkhillimage = new JLabel(imageIcon);
+                parkhillimage.setBounds(200, 200, 871, 350);
+                
+        }
+}
+                
+                
+                
+
+		
+                               
+	
+
+
