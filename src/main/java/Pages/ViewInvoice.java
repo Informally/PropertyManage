dd141fd0aeec23809dd287a792814ac9bc8024a0
B3njam1n3012 <tankerkzheng1@gmail.com>
2023-02-27 12:48:08 +0800

    Update ViewInvoice.java

--- src/main/java/Pages/ViewInvoice.java
+++ src/main/java/Pages/ViewInvoice.java
@@ -224,7 +224,8 @@ public class ViewInvoice extends PageUtils {
 				int columnNum = jTablePayment.getColumnCount();
 
 				if (row.length > 0) {
-
+				//clear text area
+				textArea.setText("");
 				String paymentID = (String) jTablePayment.getValueAt(row[0], 0);
 				String type = (String) jTablePayment.getValueAt(row[0], 1);
 				String type1 = (String) jTablePayment.getValueAt(row[0], 2);
@@ -256,6 +257,8 @@ public class ViewInvoice extends PageUtils {
 					String receiptReport = "";
 					
 					for (ArrayList<String> row : statementData) {
+						//clear text area
+						textArea.setText("");
 						if(row.get(3).equals("January")) {
 						receiptReport += "\n\n\n\n\n\n\n\n\n\n\n\n\n\n          " + row.get(1) + "\t" + row.get(2) + "\t" + row.get(3) + "\t " + row.get(4) + "\t           " + row.get(5);
 						lblSDate.setText("Date Issued:     2023-01-01");
@@ -650,80 +653,7 @@ public class ViewInvoice extends PageUtils {
                        
 		});
 
-		/*CRUD newCrud = new CRUD();
-		tblData.clear();
-		tblData = newCrud.read("Payment.txt");
-		String rowPayment[] = new String[7];
-		String columnPayment[] = { "Id", "Name", "Detail", "Payment", "Role", "Debt ID", "Date" };
 
-		JTable jTablePayment = new JTable();
-		jTablePayment.setBounds(44, 73, 770, 100);
-		DefaultTableModel tableModelPayment = (DefaultTableModel) jTablePayment.getModel();
-		tableModelPayment.setColumnIdentifiers(columnPayment);
-		jTablePayment.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		System.out.print(tblData);
-
-		for (int i = 0; i < tblData.size(); i++) {
-			rowPayment[0] = tblData.get(i).get(0);
-			rowPayment[1] = tblData.get(i).get(1);
-			rowPayment[2] = tblData.get(i).get(2);
-			rowPayment[3] = tblData.get(i).get(3);
-			rowPayment[4] = tblData.get(i).get(4);
-			rowPayment[5] = tblData.get(i).get(5);
-			rowPayment[6] = tblData.get(i).get(6);
-			tableModelPayment.addRow(rowPayment);
-
-		}
-		jTablePayment.setModel(tableModelPayment);
-
-		
-		JScrollPane scrollPayment = new JScrollPane(jTablePayment, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scrollPayment.setBounds(44, 73, 770, 100);
-		viewInvoice.getContentPane().add(scrollPayment);
-		
-		
-		// Result Display
-		JTextArea textArea = new JTextArea();
-		textArea.setLineWrap(true);
-		textArea.setEditable(false);
-		textArea.setBounds(44, 200, 770, 300);
-
-
-		// Scroll
-		JScrollPane scroll = new JScrollPane (textArea, 
-				   JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scroll.setBounds(44, 200, 770, 300);
-		viewInvoice.getContentPane().add(scroll);
-
-		ListSelectionModel selectPayment = jTablePayment.getSelectionModel();
-		selectPayment.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		selectPayment.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent e) {
-				int[] row = jTablePayment.getSelectedRows();
-				int columnNum = jTablePayment.getColumnCount();
-
-				if (row.length > 0) {
-
-				String paymentID = (String) jTablePayment.getValueAt(row[0], 0);
-				String name = (String) jTablePayment.getValueAt(row[0], 1);
-				String detail = (String) jTablePayment.getValueAt(row[0], 2);
-				String payment = (String) jTablePayment.getValueAt(row[0], 3);
-				String role = (String) jTablePayment.getValueAt(row[0], 4);
-				String debtID = (String) jTablePayment.getValueAt(row[0], 5);
-				String date = (String) jTablePayment.getValueAt(row[0], 6);
-
-
-				String report = "<-------- Invoice/Receipt -------->\n\n";
-					
-				report += "\nPayment Id: " + paymentID + "\n" + "Name: " + name + "\n" + "Detail: " + detail + "\n" + "Payment: RM" + payment + "\n" + "Role: " + role + "\n"+ "Date: " + date + "\n----------------------\n";
-				textArea.setText(report);
-
-				}
-				
-
-			}
-		});*/
 		
 		// back Button
 		JButton backBtn = new JButton("Back");
